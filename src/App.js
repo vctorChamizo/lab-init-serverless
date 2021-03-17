d738c66d843e53a0dc6b46c4f8af0e70d663fb1c
vctorChamizo <victor.chamizo96@gmail.com>
2021-03-17 17:23:21 +0100

    fix: react bug

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { useState } from 'react'
 import ApolloClient from 'apollo-boost'
+import { gql, InMemoryCache } from '@apollo/client'
 
 import './App.css'
+
 import logo from './logo.svg'
 
 const client = new ApolloClient({
-  uri: '/.netlify/functions/graphql'
+  uri: '/.netlify/functions/graphql',
+  cache: new InMemoryCache()
 })
+
+const helloQuery = gql`
+  query {
+    hello
+  }
+`
 
 const LambdaDemo = () => {
   const [loading, setLoading] = useState(false)
   const [message, setMessage] = useState(null)
 
-  const handleClick = () => {
-    setLoading(true)
-    fetch('/.netlify/functions/')
-      .then((response) => response.json())
-      .then((json) => {
-        setMessage(json.msg)
-        setLoading(false)
+  const handleClick = async () => {
+    try {
+      setLoading(true)
+
+      // const response = await fetch('/.netlify/functions/graphql')
+      // const { url } = response
+      // const newMessage = url || 'Ha ocurrido un error, vuelva a intentarlo.'
+
+      const response = await client.query({
+        query: helloQuery
       })
+
+      console.log(response)
+
+      return null
+
+      // setMessage(newMessage)
+      // setLoading(false)
+    } catch (error) {
+      return null
+    }
   }
 
   return (
@@ -38,9 +60,6 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <LambdaDemo />
       </header>
     </div>
